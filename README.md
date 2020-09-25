7de876c8601ccb0e9c81d83c067d2aa35e40d89a
Yuuna233 <66722623+Yuuna233@users.noreply.github.com>
2020-09-25 21:17:54 +0800

    Q1

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # my-LeetCode-in-Java
 个人LeetCode刷题记录
+
+## Solutions
+
+- [Q1:Two sum](/src/两数之和)
+
---
+++ src/Two Sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static int[] twoSum(int[] nums, int target) {
+        int[] output={-1,-1};
+        for(int n1=0; n1<nums.length; n1++){
+            for(int n2=n1+1; n2<nums.length; n2++){
+                if(nums[n1]+nums[n2]==target){
+                    output[0]=n1;
+                    output[1]=n2;
+                }
+            }
+        }
+        return output;
+    }
+}
